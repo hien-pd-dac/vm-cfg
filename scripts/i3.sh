960d264649a83e96dbecf9363285d47efd2aa169
hien-pd-dac <dh.pham.dev@gmail.com>
2023-05-09 08:05:28 +0900

    feat(i3): change backgound

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -13,10 +13,16 @@ install_i3() {
   sudo apt install i3blocks feh pavucontrol arandr rofi
   mkdir -p "$HOME"/.config/i3/
   ln -s -f "$VM_CFG_PATH"/files/i3/config "$HOME"/.config/i3/config
+  ln -s -f "$VM_CFG_PATH"/files/i3/gnome-background.svg \
+     "$HOME"/.config/i3/gnome-background.svg
+  
 
   # requirements
   # see also: https://bumblebee-status.readthedocs.io/en/main/modules.html
-  git clone --depth 1 https://github.com/tobi-wan-kenobi/bumblebee-status.git "$HOME"/.config/i3/bumblebee-status/
+  if [[ ! -d "$HOME"/.config/i3/bumblebee-status ]]; then
+    git clone --depth 1 https://github.com/tobi-wan-kenobi/bumblebee-status.git \
+      "$HOME"/.config/i3/bumblebee-status/
+  fi
   ## cpu
   sudo apt install python3-pip
   sudo apt install pipx
